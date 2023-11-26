a96f3e3c341b9893783a3a8aaed75881c5ae68ac
Alla Borisova <alla.borisova27@gmail.com>
2023-11-26 12:06:15 +0500

    add main page with adaptive

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,26 +1,7 @@
 Fancybox.bind('[data-fancybox]', {});
 
-const swiper_expedition = new Swiper('.expedition-event__slider', {
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-const swiper_education = new Swiper('.education__slider', {
-  loop: true,
-  slidesPerView: 1,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
-
 jQuery(document).ready(function ($) {
   $('.tabs').lightTabs();
-  $('.category__kitchen__tabs').lightTabs();
 
   $('[type=tel]').mask('+7 (999) 999-99-99');
 
@@ -29,101 +10,99 @@ jQuery(document).ready(function ($) {
     $('body').toggleClass('open-menu');
   });
 
-  $('.service__how-caption').click(function () {
-    $(this).parent().toggleClass('active');
-  });
-
-  $('.reviews-page-slider-for').slick({
+  $('.main-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
     slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.reviews-page-slider-nav',
-  });
-  $('.reviews-page-slider-nav').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    asNavFor: '.reviews-page-slider-for',
-    dots: false,
-    arrows: true,
     centerMode: true,
-    centerPadding: '0px',
-    focusOnSelect: true,
-    prevArrow: "<button type='button' class='slick-prev-review'></button>",
-    nextArrow: "<button type='button' class='slick-next-review'></button>",
-    // prevArrow: '',
-    // nextArrow: '',
+    variableWidth: true,
     responsive: [
       {
-        breakpoint: 993,
+        breakpoint: 768,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 481,
-        settings: {
+          arrows: false,
+          centerMode: false,
+
           slidesToShow: 1,
-          slidesToScroll: 1,
+          variableWidth: false,
         },
       },
     ],
   });
 
-  $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav',
-  });
-  $('.slider-nav').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: false,
-    arrows: true,
-    centerMode: true,
-    centerPadding: '0px',
-    focusOnSelect: true,
-    vertical: true,
-    prevArrow:
-      '<button type="button" class="slick-prev-recomm"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg></button>',
-    nextArrow:
-      '<button type="button" class="slick-next-recomm"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg></button>',
-    // prevArrow: '',
-    // nextArrow: '',
-    responsive: [
-      {
-        breakpoint: 993,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 769,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          vertical: false,
-        },
-      },
-      {
-        breakpoint: 481,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  });
+  // $('.main-slider').slick({
+  //   centerMode: true,
+  //   centerPadding: '60px',
+  //   slidesToShow: 1,
+  //   responsive: [
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         arrows: false,
+  //         centerMode: true,
+  //         centerPadding: '40px',
+  //         slidesToShow: 3,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         arrows: false,
+  //         centerMode: true,
+  //         centerPadding: '40px',
+  //         slidesToShow: 1,
+  //       },
+  //     },
+  //   ],
+  // });
+
+  // $('.slider-for').slick({
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   arrows: false,
+  //   fade: true,
+  //   asNavFor: '.slider-nav',
+  // });
+  // $('.slider-nav').slick({
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   asNavFor: '.slider-for',
+  //   dots: false,
+  //   arrows: true,
+  //   centerMode: true,
+  //   centerPadding: '0px',
+  //   focusOnSelect: true,
+  //   vertical: true,
+  //   prevArrow:
+  //     '<button type="button" class="slick-prev-recomm"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg></button>',
+  //   nextArrow:
+  //     '<button type="button" class="slick-next-recomm"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg></button>',
+  //   // prevArrow: '',
+  //   // nextArrow: '',
+  //   responsive: [
+  //     {
+  //       breakpoint: 993,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 769,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 1,
+  //         vertical: false,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 481,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //   ],
+  // });
 });
