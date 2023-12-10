477d34f8293bd8bddc6870075a591862009471e9
Alla Borisova <alla.borisova27@gmail.com>
2023-12-10 13:12:56 +0500

    feat: 3 pages, tabs, change menu

--- build/js/main.js
+++ build/js/main.js
@@ -5,6 +5,29 @@ jQuery(document).ready(function ($) {
 
   $('[type=tel]').mask('+7 (999) 999-99-99');
 
+  $('.sub-menu-link').click(function (e) {
+    e.preventDefault();
+    $('.sub-menu-link').removeClass('open');
+    $(this).addClass('open');
+    $('body').addClass('open-menu');
+  });
+
+  // window.addEventListener('click', function (e) {
+  //   if (!menu.contains(e.target) && !button.contains(e.target)) {
+  //     // Ниже код, который нужно выполнить при срабатывании события.
+  //     menu.classList.add('hide');
+  //   }
+  // });
+  $(document).mouseup(function (e) {
+    const container = $('.nav-menu');
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      $('.sub-menu-link').removeClass('open');
+      if (!$('.hamburger').hasClass('is-active')) {
+        $('body').removeClass('open-menu');
+      }
+    }
+  });
+
   $('.hamburger').click(function () {
     $('.hamburger').toggleClass('is-active');
     $('body').toggleClass('open-menu');
